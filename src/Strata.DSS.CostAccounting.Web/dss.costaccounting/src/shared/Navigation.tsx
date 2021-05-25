500781e06c2c31a8256ddfc8936470c8c3f76e29
Feras Sweis <fsweis@stratadecision.com>
2021-05-25 14:40:31 -0500

    Cleanup example files

--- src/Strata.DSS.CostAccounting.Web/dss.costaccounting/src/shared/Navigation.tsx
+++ src/Strata.DSS.CostAccounting.Web/dss.costaccounting/src/shared/Navigation.tsx
@@ -4,8 +4,6 @@ import Header from '@strata/tempo/lib/header';
 import Layout from '@strata/tempo/lib/layout';
 import Menu from '@strata/tempo/lib/menu';
 import { Switch, Route } from 'react-router-dom';
-import ExampleUser from '../user-example/ExampleUser';
-import ExampleDatabase from '../database-example/ExampleDatabase';
 import StatisticDrivers from '../statisticdrivers/StatisticDrivers';
 import { Navbar } from '@strata/navbar/lib';
 
@@ -30,13 +28,7 @@ const Navigation: React.FC = () => {
           <Layout.Sider collapsible>
             <Header title='Cost Accounting' />
             <Menu selectedKeys={getActiveUrlKey()}>
-              <Menu.ItemGroup title='Menu Group 1'>
-                <Menu.Item key='/users' href='/users'>
-                  Users
-                </Menu.Item>
-                <Menu.Item key='/databases' href='/databases'>
-                  Databases
-                </Menu.Item>
+              <Menu.ItemGroup title='CATEGORIZE'>
                 <Menu.Item key='/statistic-drivers' href='/statistic-drivers'>
                   Statistic Drivers
                 </Menu.Item>
@@ -45,9 +37,7 @@ const Navigation: React.FC = () => {
           </Layout.Sider>
           <Layout.Content>
             <Switch>
-              <Route path={['/', '/users']} exact component={ExampleUser} key='users'></Route>
-              <Route path='/databases' exact component={ExampleDatabase} key='databases'></Route>
-              <Route path='/statistic-drivers' exact component={StatisticDrivers} key='statistic-drivers'></Route>
+              <Route path={['/', '/statistic-drivers']} exact component={StatisticDrivers} key='statistic-drivers'></Route>
             </Switch>
           </Layout.Content>
         </Layout>
