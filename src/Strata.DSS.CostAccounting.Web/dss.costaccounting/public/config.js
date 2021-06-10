c09f9cff17da652b5bb198dcab5c44d0a3133cd5
Carmen Bello <cbello@stratadecision.com>
2021-06-10 11:35:15 -0500

    update config.js

--- src/Strata.DSS.CostAccounting.Web/dss.costaccounting/public/config.js
+++ src/Strata.DSS.CostAccounting.Web/dss.costaccounting/public/config.js
@@ -1,6 +1,6 @@
 window['config'] = {
   appName: 'dss-costaccounting',
-  apiUrl: 'https://costaccounting-api.dev.stratanetwork.net/api/v1/',
+  apiUrl: 'https://localhost:8443/api/v1/',
   identityServerUrl: 'https://identity.dev.stratanetwork.net/',
   menuUrl: 'https://menu.dev.stratanetwork.net/api/v1/Menu/',
   env: 'local',
