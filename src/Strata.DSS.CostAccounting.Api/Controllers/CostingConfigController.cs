08c848b78e50a476033613b0cfe2d994bcdb9d80
Mike Schmidt <mschmidt@stratadecision.com>
2021-06-17 16:25:23 -0500

    thwart the hackers

--- src/Strata.DSS.CostAccounting.Api/Controllers/CostingConfigController.cs
+++ src/Strata.DSS.CostAccounting.Api/Controllers/CostingConfigController.cs
@@ -1,16 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Strata.DSS.CostAccounting.Biz.CostAccounting.Models;
 using Strata.DSS.CostAccounting.Biz.CostAccounting.Repositories;
+using Strata.DSS.CostAccounting.Biz.CostingConfigs.Models;
+using Strata.DSS.CostAccounting.Biz.CostingConfigs.Services;
 using Strata.DSS.CostAccounting.Biz.Enums;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Strata.ApiLib.Standard.Models;
-using Strata.DSS.CostAccounting.Biz.CostingConfigs.Services;
-using Strata.DSS.CostAccounting.Biz.CostingConfigs.Models;
-using System.Linq;
 
 namespace Strata.DSS.CostAccounting.Api.Controllers
 {
@@ -52,6 +49,9 @@ namespace Strata.DSS.CostAccounting.Api.Controllers
         [ProducesResponseType(200)]
         public async Task<IEnumerable<Entity>> GetEntities(CancellationToken cancellationToken)
         {
+            var isCostingEntityLevelSecurityEnabled = await _systemSettingRepository.GetIsCostingEntityLevelSecurityEnabledAsync(cancellationToken);
+            if (isCostingEntityLevelSecurityEnabled) { return new List<Entity>(); }
+
             var entities = await _entityService.GetEntities(cancellationToken);
             return entities;
         }
