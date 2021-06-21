98fa9ef44798a45127a783291dbf4e932c1e2576
Feras Sweis <fsweis@stratadecision.com>
2021-06-21 14:41:08 -0500

    cleanup

--- src/Strata.DSS.CostAccounting.Biz/CostAccounting/Repositories/ICostAccountingRepository.cs
+++ src/Strata.DSS.CostAccounting.Biz/CostAccounting/Repositories/ICostAccountingRepository.cs
@@ -8,8 +8,6 @@ namespace Strata.DSS.CostAccounting.Biz.CostAccounting.Repositories
 {
     public interface ICostAccountingRepository
     {
-        public Task<IEnumerable<CostAccountingModel>> GetAllCostAccountingsAsync(CancellationToken cancellationToken);
-        public Task<CostAccountingModel> GetCostAccountingAsync(Guid id, CancellationToken cancellationToken);
         public Task<IList<DataTable>> GetDataTablesAsync(IList<string> globalIds, CancellationToken cancellationToken);
         public Task<IList<Measure>> GetMeasuresAsync(IList<Guid> dataTableGuids, CancellationToken cancellationToken);
         public Task<IList<RuleEngineIncludedMeasure>> GetRuleEngineIncludedMeasuresAsync(CancellationToken cancellationToken);
